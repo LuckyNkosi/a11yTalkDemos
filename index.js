2fe1caa5c0fcebc307d6926d0ffc4fce7b2bcdd7
Lucky Nkosi <lucky@bbd.co.za>
2023-03-14 19:43:57 +0200

    built the specific site tester

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const pa11y = require('pa11y');
+const baseSites = require('./baseSites.json');
+const PORT = process.env.PORT || 3003;
+
+const app = express();
+app.use(cors());
+// Array of sites to test
+const sites = baseSites;
+app.use(express.static('public'));
+
+app.get('sitesTested', (req, res) => {
+    res.json(sites);
+});
+
+app.get('/testSite', async (req, res) => {
+    console.log('test called');
+    const url = req.query.url;
+    const result = await pa11y(url);
+    res.json(result);
+});
+
+app.post('/addSite', (req, res) => {
+    sites.push(req.query.url);
+    res.json(sites);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
---
+++ public/script.js
@@ -0,0 +1,39 @@
+const url = document.getElementById('site');
+
+const resultsBody = document.getElementById('resultsTableBody');
+const getSitesToTest = async () => {
+    const response = await fetch('sitesTested');
+    const sites = await response.json();
+    return sites;
+}
+
+const testSite = async () => {
+    url.checkValidity();
+    url.reportValidity();
+    console.log(url.validity);
+    if (!url.validity.valid)
+        return;
+
+    const response = await fetch(`http://localhost:3003/testSite?url=${url.value}`);
+    const result = await response.json();
+    console.log('====================================');
+    console.log(result);
+    console.log('====================================');
+    const createTableRow = (result) => {
+        const tr = document.createElement('tr');
+        const cols = ['code', 'type', 'message', 'context'];
+        cols.forEach(col => {
+            const td = document.createElement('td');
+            td.innerText = result[col];
+            tr.appendChild(td);
+        });
+        if (result.type === 'error')
+            tr.classList.add('error');
+        return tr;
+    }
+    result.issues.forEach(r => {
+        //insert each entry of result into table
+        const tr = createTableRow(r);
+        resultsBody.appendChild(tr);
+    });
+}
